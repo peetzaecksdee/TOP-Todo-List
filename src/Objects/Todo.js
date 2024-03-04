9476cdb4de74ed8516127daf2930cb8f8e20e0a3
peetzaecksdee <peetzaecksdee@skiff.com>
2024-03-04 13:03:17 +0700

    Updated Object Constructor in Todo

--- src/Objects/Todo.js
+++ src/Objects/Todo.js
@@ -3,13 +3,13 @@
  * @param {number} id 
  * @param {string} title 
  * @param {Date} date 
- * @param {Date} dueDate 
- * @param {Array} steps 
- * @param {string} notes 
+ * @param {Date?} dueDate 
+ * @param {Array<string>} steps 
+ * @param {string?} notes 
  * @param {boolean} starred 
  */
 
-export default function(id, title, date, dueDate, steps, notes, starred) {
+export default function(id=1, title, date, dueDate=null, steps=[], notes="", starred=false) {
   this.id = id;
   this.title = title;
   this.date = date;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import Todo from './Objects/Todo'
 
+const newTodo = new Todo(1, "a", Date.now());
+console.log(newTodo);
 console.log("hello guys. what is this project?");
