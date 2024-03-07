d410001dae766df713fae6b54634aa97897342b7
peetzaecksdee <peetzaecksdee@skiff.com>
2024-03-07 17:58:13 +0700

    Target = "_blank";

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,33 +1,36 @@
-{/* <footer>
+{
+	/* <footer>
   <span>Another day, Another project</span>
   <a href="https://github.com/peetzaecksdee">
     <img src="./assets/images/pizza.jpg" alt="my profile" />
   </a>
-</footer> */}
+</footer> */
+}
 
-import './Footer.css';
+import "./Footer.css";
 
 function loadFooter() {
-  const footer = document.createElement('footer');
+	const footer = document.createElement("footer");
 
-  const text = document.createElement('i');
-  text.textContent = 'Another day, Another project';
-  text.classList.add('credit')
+	const text = document.createElement("i");
+	text.textContent = "Another day, Another project";
+	text.classList.add("credit");
 
-  const githubPage = document.createElement('a');
-  githubPage.href = 'https://github.com/peetzaecksdee';
-  githubPage.classList.add('credit-a')
+	const githubPage = document.createElement("a");
+	githubPage.href = "https://github.com/peetzaecksdee";
+	githubPage.classList.add("credit-a");
+  githubPage.target = "_blank";
 
-  const githubImage = document.createElement('img');
-  githubImage.src = './assets/images/pizza.jpg';
-  githubImage.alt = 'my profile';
+	const githubImage = document.createElement("img");
+	githubImage.src = "./assets/images/pizza.jpg";
+	githubImage.alt = "my profile";
 
-  githubPage.appendChild(githubImage);
+	githubPage.appendChild(githubImage);
 
-  footer.appendChild(text);
-  footer.appendChild(githubPage);
+	footer.appendChild(text);
+	footer.appendChild(githubPage);
 
-  return footer
+	return footer;
 }
 
 export default loadFooter;
