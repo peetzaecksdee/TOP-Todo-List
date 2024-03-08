59d4c5fc5a21ec0883c5366c3d78d1c63c319976
peetzaecksdee <peetzaecksdee@skiff.com>
2024-03-08 12:38:19 +0700

    Little path changes

---
+++ src_assets_images_pizza_jpg.main.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunktop_todo_list"] = self["webpackChunktop_todo_list"] || []).push([["src_assets_images_pizza_jpg"],{
+
+/***/ "./src/assets/images/pizza.jpg":
+/*!*************************************!*\
+  !*** ./src/assets/images/pizza.jpg ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "4884b133f5632a4289fc.jpg";
+
+/***/ })
+
+}]);
